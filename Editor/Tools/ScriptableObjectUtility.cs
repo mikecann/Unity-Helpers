391a15690361ae9ef2a5aa47f6f5ba5a652c396c
Michael Cann <mike.cann@gmail.com>
2014-07-10 14:04:09 +0800

    work on a tool to create "Data" objects

--- Editor/Tools/ScriptableObjectUtility.cs
+++ Editor/Tools/ScriptableObjectUtility.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Scripts.Game.Views.Data;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,22 +9,61 @@ using UnityEngine;
 
 namespace Assets.Libraries.UnityHelpers.Editor.Tools
 {
-    public class ScriptableObjectUtility
+    public class ScriptableObjectUtility : EditorWindow
     {
-        //[MenuItem("UnityHelpers/Create Asset")]
-        //public static void CreateAsset()
+        private bool isOpen;
+
+        [MenuItem("UnityHelpers/Create Asset")]
+        public static void CreateAsset()
+        {
+            GetWindow<ScriptableObjectUtility>();
+        }
+
+        //[MenuItem("UnityHelpers/Create Asset 2")]
+        //public static void CreateAsset2()
         //{
-        //    EditorGUIUtility.ShowObjectPicker<MonoScript>(null, false, "", 1234);
-        //    //ScriptableObjectUtility.CreateAsset<UnityHelpersSettings>();
-        //}
+        //    CreateAsset<LevelsData>();
+        //}    
+
+        void OnGUI()
+        {
+            if (!isOpen)
+            {
+                int controlID = EditorGUIUtility.GetControlID(FocusType.Passive);
+                EditorGUIUtility.ShowObjectPicker<MonoScript>(null, false, "", controlID);
+                isOpen = true;
+            }
+            if (Event.current.commandName == "ObjectSelectorClosed")
+            {
+                var script = (MonoScript)EditorGUIUtility.GetObjectPickerObject();
+                if(script!=null)
+                {
+                    var path = AssetDatabase.GetAssetOrScenePath(script);
+                    var classname = Path.GetFileNameWithoutExtension(path);
+                    var type = AppDomain.CurrentDomain.GetAssemblies()
+                                   .SelectMany(x => x.GetTypes())
+                                   .FirstOrDefault(x => x.Name == classname);
+                    if (type == null) Debug.LogError("Could not find type with class name: "+classname);
+                    else CreateAsset(type);
+                    Close();
+                }
+            }
+        }
+
+        public static void CreateAsset<T>() where T : ScriptableObject
+        {
+            CreateAsset(typeof(T));
+        }
 
         /// <summary>    
         /// This makes it easy to create, name and place unique new ScriptableObject asset files.
         /// Borrowed from: http://wiki.unity3d.com/index.php/CreateScriptableObjectAsset
         /// </summary>
-        public static void CreateAsset<T>() where T : ScriptableObject
+        public static void CreateAsset(Type t)
         {
-            T asset = ScriptableObject.CreateInstance<T>();
+            if (!t.IsSubclassOf(typeof(ScriptableObject))) throw new Exception("Asset must a be a scriptable object!");
+
+            var asset = ScriptableObject.CreateInstance(t);
 
             string path = AssetDatabase.GetAssetPath(Selection.activeObject);
             if (path == "")
@@ -35,7 +75,7 @@ namespace Assets.Libraries.UnityHelpers.Editor.Tools
                 path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
             }
 
-            string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New " + typeof(T).ToString() + ".asset");
+            string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New " + t.ToString() + ".asset");
 
             AssetDatabase.CreateAsset(asset, assetPathAndName);
 
