fc2fe9c4767fc887c50e640d3d188a7fa8d9d541
Mike Cann <mike.cann@gmail.com>
2014-12-12 10:10:35 +0000

    Added a hack to open the sprite editor for a given texture

---
+++ Editor/Tools/MiscHacks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace Assets.Libraries.UnityHelpers.Editor.Tools
+{
+    public class MiscHacks
+    {
+        public static void OpenSpriteEditor(string texturePath)
+        {
+            // Lookup the Editor assembly
+            if (typeof(EditorGUIUtility).Assembly == null)
+                throw new Exception("Cannot find the EditorGUIUtility Assembly therefore cannot search for SpriteEditorWindow");
+
+            // Look for the internal class
+            var type = typeof(EditorGUIUtility).Assembly.GetType("UnityEditor.SpriteEditorWindow");
+            if (type == null) 
+                throw new Exception("Cannot find the SpriteEditorWindow class!");
+
+            // Get the call to open the window
+            var openMethod = type.GetMethod("GetWindow");
+            if (openMethod == null) 
+                throw new Exception("Cannot find the GetWindow method");
+
+            // Select the texture
+            Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(texturePath);
+
+            // Finally open it
+            openMethod.Invoke(null, null);
+        }
+    }
+}
