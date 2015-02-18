9e48753047b008911f2f95aa2348c07973c9f6b4
Mike Cann <mike.cann@gmail.com>
2015-02-18 16:35:16 +0800

    added a short form of the readable string extension to TimeSpan

--- Scripts/UnityExtensions.cs
+++ Scripts/UnityExtensions.cs
@@ -341,6 +341,26 @@ public static class UnityExtensions
     }
 
     /// <summary>
+    /// Converts a timespan to a readable format but in a shorter form
+    /// </summary>
+    /// <param name="span"></param>
+    /// <returns></returns>
+    public static string ToReadableStringShortForm(this TimeSpan span)
+    {
+        string formatted = string.Format("{0}{1}{2}{3}",
+            span.Duration().Days > 0 ? string.Format("{0:0}d, ", span.Days) : string.Empty,
+            span.Duration().Hours > 0 ? string.Format("{0:0}h, ", span.Hours) : string.Empty,
+            span.Duration().Minutes > 0 ? string.Format("{0:0}m, ", span.Minutes) : string.Empty,
+            span.Duration().Seconds > 0 ? string.Format("{0:0}s", span.Seconds) : string.Empty);
+
+        if (formatted.EndsWith(", ")) formatted = formatted.Substring(0, formatted.Length - 2);
+
+        if (string.IsNullOrEmpty(formatted)) formatted = "0s";
+
+        return formatted;
+    }
+
+    /// <summary>
     /// Randomly picks one elements from the enumerable
     /// </summary>
     /// <typeparam name="T">The type of the item</typeparam>
